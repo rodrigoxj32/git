df1dc3316e4688d607400f3e152068164dd871d8
Rodrigo Daniel Segovia <rodrigoxj32@hotmail.com>
2018-03-10 17:47:37 -0600

    Iniciando proyecto de java

---
+++ src/cursogit/CursoGit.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursogit;
+
+/**
+ *
+ * @author Daniel
+ */
+
+import java.util.Scanner;
+
+public class CursoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Persona persona = new Persona();
+        
+        persona.setApellidos("Rodrigo");
+                
+        System.out.println(persona.toString());
+        
+      
+        
+    }
+    
+}
